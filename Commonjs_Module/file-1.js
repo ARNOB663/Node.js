c0df8cd3a13383e504b0eb1449d6daea8416656d
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-31 18:37:01 +0600

    update

--- Commonjs_Module/file-1.js
+++ Commonjs_Module/file-1.js
@@ -1,5 +1,9 @@
-const {a,add,b} = require("./file-2.js")
+const {a,add,b} = require("./file-2.js");
+const {a:a3,add: add3,b:b3} = require("./file-3.js");
 
 console.log(a);
 console.log(add(2,3));
 console.log(b);
+console.log(a3);
+console.log(add3(2,3,4));
+console.log(b3);
