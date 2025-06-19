f59a65adda7e93c49b26aa3b2ee861ac48eac111
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-06-19 12:42:29 +0600

    Update app.ts

--- Note App with mongoose/src/app.ts
+++ Note App with mongoose/src/app.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { Schema  } from "mongoose";
 const app:Application = express();
 
+app.use(express.json())
 
 //mongoose model
 const noteSchema = new Schema({
@@ -32,22 +33,40 @@ tags:{
 
 const Note = mongoose.model("Note",noteSchema)
 
-app.post('/create-note',async(req:Request,res:Response)=>{
-    const myNote = new Note({
-        title: "Learning express",
-        tags:{
-            label:"database",
+app.post('/notes/create-note',async(req:Request,res:Response)=>{
 
-        }
+   const body = req.body
+
+   //aproch 1 of creating a data 
+    // const myNote = new Note({
+    //     title: "Learning express",
+    //     tags:{
+    //         label:"database",
+
+    //     }
         
         
-    })
-    await myNote.save()
+    //})
+   // await myNote.save()
+
+   //Approch -2
+   const note = await Note.create(body)
 
     res.status(201).json({
         success:true,
         message:"Note Created successfuly",
-        note:myNote
+        note:body
+    })
+
+})
+app.get('/notes',async(req:Request,res:Response)=>{
+
+    const notes = await Note.find()
+   
+    res.status(201).json({
+        success:true,
+        message:"Note Created successfuly",
+        notes:notes
     })
 
 })
